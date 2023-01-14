7b6d330fe0f3d6a771ac5f98f36a54455605bf0e
subhajit5286 <subhajit.bhattacharyya@gmail.com>
2023-01-14 20:44:36 +0530

    6th assgnment

--- dom.js
+++ dom.js
@@ -15,8 +15,13 @@ listitems[2].style.backgroundColor='green';
 for(var i=0;i<listitems.length;i++){
     listitems[i].style.fontWeight='bold';
 }
-console.log('sb')
+console.log('listitems')
 var li=document.getElementsByTagName('li');
 console.log(li)
 li[4].textContent='Last item';
 li[4].style.backgroundColor='pink'
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='red'
+console.log('secondItem')
+var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.visibility='hidden'
